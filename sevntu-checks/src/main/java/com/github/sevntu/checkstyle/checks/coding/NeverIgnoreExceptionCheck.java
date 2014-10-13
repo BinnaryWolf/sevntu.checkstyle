059ae2fbef9b4a08b56df0f209d41078184710ad
binnarywolf <BinnaryWolf@gmail.com>
2014-10-14 02:46:17 +0400

    Add full tests for NeverIgnorException

--- sevntu-checks/src/main/java/com/github/sevntu/checkstyle/checks/coding/NeverIgnoreExceptionCheck.java
+++ sevntu-checks/src/main/java/com/github/sevntu/checkstyle/checks/coding/NeverIgnoreExceptionCheck.java
@@ -6,16 +6,48 @@ import com.puppycrawl.tools.checkstyle.api.TokenTypes;
 
 public class NeverIgnoreExceptionCheck extends Check
 {
+    /**
+     * Warning message key.
+     */
+    public final static String MSG_KEY = "ignore.exception";
+    /**
+     * Default isCommentAllowed
+     */
+    public final static boolean DEFAULT_IS_COMMENT_ALLOWED = true;
+
+    /**
+     * Default regexp for exception classname
+     */
+    public final static String DEFAULT_EXCEPTION_CLASSNAME_REGEXP = "InterruptedException";
+
+    private boolean mIsCommentAllowed;
+    private String mExceptionClassNameRegexp;
+
+    public NeverIgnoreExceptionCheck()
+    {
+        mIsCommentAllowed = DEFAULT_IS_COMMENT_ALLOWED;
+        mExceptionClassNameRegexp = DEFAULT_EXCEPTION_CLASSNAME_REGEXP;
+    }
+
+    public void setIsCommentAllowed(boolean aAllow)
+    {
+        mIsCommentAllowed = aAllow;
+    }
+
+    public void setExceptionClassNameRegexp(String aRegexp)
+    {
+        mExceptionClassNameRegexp = aRegexp;
+    }
 
     @Override
     public int[] getDefaultTokens()
     {
-        return new int [] {TokenTypes.LITERAL_CATCH};
+        return new int[] { TokenTypes.LITERAL_CATCH };
     }
-    
+
     @Override
     public void visitToken(DetailAST aMethodNode)
     {
-        
+
     }
 }
