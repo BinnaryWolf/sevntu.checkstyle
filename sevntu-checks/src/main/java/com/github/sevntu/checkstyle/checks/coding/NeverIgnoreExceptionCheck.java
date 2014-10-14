d871ef86acdd9bd05d1f34d93de9d822adfcebbf
binnarywolf <BinnaryWolf@gmail.com>
2014-10-14 05:15:59 +0400

    Fix code style

--- sevntu-checks/src/main/java/com/github/sevntu/checkstyle/checks/coding/NeverIgnoreExceptionCheck.java
+++ sevntu-checks/src/main/java/com/github/sevntu/checkstyle/checks/coding/NeverIgnoreExceptionCheck.java
@@ -1,3 +1,21 @@
+////////////////////////////////////////////////////////////////////////////////
+// checkstyle: Checks Java source code for adherence to a set of rules.
+// Copyright (C) 2001-2014  Oliver Burn
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+////////////////////////////////////////////////////////////////////////////////
 package com.github.sevntu.checkstyle.checks.coding;
 
 import java.util.regex.Matcher;
@@ -6,27 +24,60 @@ import java.util.regex.Pattern;
 import com.puppycrawl.tools.checkstyle.api.Check;
 import com.puppycrawl.tools.checkstyle.api.DetailAST;
 import com.puppycrawl.tools.checkstyle.api.TokenTypes;
-import com.sun.mirror.util.Types;
 
+/**
+ * <p>
+ * This Check warns on non handling exceptions.<br>
+ * Examples:
+ * <p>
+ * <code><pre>
+ * try{
+ *      //some code
+ * }
+ * catch (InterruptedException e) {
+ * }
+ * <br>
+ * try{
+ *      //some code
+ * }
+ * catch (InterruptedException e) {
+ *      //No code here
+ * }
+ * </pre></code>Check could be configured to validate exceptions that match
+ * regex pattern(InterruptedException by default).<br>
+ * Also you can allow using comment to decribe why exception not handle instead
+ * of handling exception with code.
+ * @author <a href="mailto:binnarywolf@gmail.com">Dmitriy Bazunov</a>
+ */
 public class NeverIgnoreExceptionCheck extends Check
 {
     /**
      * Warning message key.
      */
-    public final static String MSG_KEY = "ignore.exception";
+    public static final String MSG_KEY = "ignore.exception";
     /**
-     * Default isCommentAllowed
+     * Default isCommentAllowed.
      */
-    public final static boolean DEFAULT_IS_COMMENT_ALLOWED = true;
+    public static final boolean DEFAULT_IS_COMMENT_ALLOWED = true;
 
     /**
-     * Default regexp for exception classname
+     * Default regex pattern for exception class name.
      */
-    public final static String DEFAULT_EXCEPTION_CLASSNAME_REGEXP = "InterruptedException";
+    public static final String DEFAULT_EXCEPTION_CLASSNAME_REGEXP = "InterruptedException";
 
+    /**
+     * If true allow to use comments instead of handling method.
+     */
     private boolean mIsCommentAllowed;
+
+    /**
+     * Regex pattern for Exception name.
+     */
     private String mExceptionClassNameRegexp;
 
+    /**
+     * Constructor with default parameters.
+     */
     public NeverIgnoreExceptionCheck()
     {
         mIsCommentAllowed = DEFAULT_IS_COMMENT_ALLOWED;
@@ -46,41 +97,60 @@ public class NeverIgnoreExceptionCheck extends Check
     @Override
     public int[] getDefaultTokens()
     {
-        return new int[] { TokenTypes.LITERAL_CATCH };
+        return new int[] {TokenTypes.LITERAL_CATCH };
     }
 
     @Override
-    public void visitToken(final DetailAST aMethodNode)
+    public void visitToken(final DetailAST aCatchNode)
     {
-        final DetailAST parameterDef = aMethodNode
+        final DetailAST parameterDef = aCatchNode
                 .findFirstToken(TokenTypes.PARAMETER_DEF);
-        final String methodName = parameterDef.findFirstToken(TokenTypes.TYPE)
+        final String exceptionName = parameterDef
+                .findFirstToken(TokenTypes.TYPE)
                 .getFirstChild().getText();
-        if (isExceptionMatchRegexp(methodName)) {
-            final DetailAST sListToken = aMethodNode
+        if (isExceptionMatchRegexp(exceptionName)) {
+            final DetailAST sListToken = aCatchNode
                     .findFirstToken(TokenTypes.SLIST);
             if (isEmptySlist(sListToken)) {
                 if (!(mIsCommentAllowed && isSListContainsComment(sListToken))) {
-                    log(aMethodNode.getLineNo(), MSG_KEY, methodName);
+                    log(aCatchNode.getLineNo(), MSG_KEY, exceptionName);
                 }
             }
         }
 
     }
 
-    private boolean isExceptionMatchRegexp(final String aMethodName)
+    /**
+     * Method check if exception name match regex pattern.
+     * @param aExceptionName
+     *        - String class name of exception to match.
+     * @return true if exception class name match and false otherwise.
+     */
+    private boolean isExceptionMatchRegexp(final String aExceptionName)
     {
         final Pattern methodNamePattern = Pattern
                 .compile(mExceptionClassNameRegexp);
-        final Matcher regexpMatcher = methodNamePattern.matcher(aMethodName);
+        final Matcher regexpMatcher = methodNamePattern.matcher(aExceptionName);
         return regexpMatcher.matches();
     }
 
+    /**
+     * Method check if catch body has no code.
+     * @param aSlistToken
+     *        - DetailAST node of AST which represent catch body.
+     * @return true if catch has no expressions and false otherwise.
+     */
     private boolean isEmptySlist(final DetailAST aSlistToken)
     {
         return aSlistToken.getFirstChild().getType() == TokenTypes.RCURLY;
     }
 
+    /**
+     * Method check if catch body has comments.
+     * @param aSlistToken
+     *        - DetailAST node of AST which represent catch body.
+     * @return true if catch body has comments and false otherwise.
+     */
     private boolean isSListContainsComment(final DetailAST aSlistToken)
     {
         final DetailAST rCurlyTocken = aSlistToken
